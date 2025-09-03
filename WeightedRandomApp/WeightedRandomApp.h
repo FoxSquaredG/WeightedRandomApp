6408ea525d627168649c0dd224bf236ecc1c97a0
foxsquaredm <foxsquaredm@gmail.com>
2025-09-03 12:02:12 +0300

    Добавьте файлы проекта.

---
+++ WeightedRandomApp/WeightedRandomApp.h
@@ -0,0 +1,32 @@
+﻿
+// WeightedRandomApp.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить pch.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CWeightedRandomAppApp:
+// Сведения о реализации этого класса: WeightedRandomApp.cpp
+//
+
+class CWeightedRandomAppApp : public CWinApp
+{
+public:
+	CWeightedRandomAppApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CWeightedRandomAppApp theApp;
